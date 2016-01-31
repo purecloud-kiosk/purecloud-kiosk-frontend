3f753ee665ee554a25f9093d98e227944d5facda
Charlie Duong <charlieduong94@gmail.com>
2016-01-30 23:37:37 -0500

    Switched to node-http-proxy
    Made the switch because syntax is cleaner and because it allowed file uploads to be proxied.

--- server.js
+++ server.js
@@ -3,61 +3,60 @@
  *
  *  For anything other than accessing the web app is forwarded the api server.
  **/
-var express = require("express");
-var bodyParser = require("body-parser");
-var request = require("request");
-var compression = require("compression");
-var favicon = require("serve-favicon");
-var apiServer = "http://localhost:8080";
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var httpProxy = require('http-proxy');
+var url = require('url');
+
+var apiServer = 'http://localhost:8080';
+
+var proxy = httpProxy.createProxyServer();
 
 var app = express();
-app.use(favicon(__dirname + "/img/favicon-bar-chart.ico"));
+app.use(favicon(__dirname + '/img/favicon-bar-chart.ico'));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.get("/", function(req, res){
+app.get('/', function(req, res){
   var token = req.query.client_token;
   if(token !== undefined){
     request({
-      "url" : "https://apps.mypurecloud.com/api/v2/session",
-      "method" : "GET",
-      "headers" : {
-        "Authorization" : "bearer " + token
+      'url' : 'https://apps.mypurecloud.com/api/v2/session',
+      'method' : 'GET',
+      'headers' : {
+        'Authorization' : 'bearer ' + token
       }
     }, function(error, response, body){
       if(error || response.statusCode == 200){
-        res.sendFile(__dirname + "/views/index.html")
+        res.sendFile(__dirname + '/views/index.html')
       }
       else{
-        res.sendFile(__dirname + "/views/login.html");
+        res.sendFile(__dirname + '/views/login.html');
       }
     });
   }
   else{
-    res.sendFile(__dirname + "/views/login.html");
+    res.sendFile(__dirname + '/views/login.html');
   }
 });
 
-app.use("/dist", express.static(__dirname + "/dist"));
+app.use('/dist', express.static(__dirname + '/dist'));
 
 /**
  *  Forward all other requests to api server
  */
-app.use("/api/*", function(req, res){
-  console.log(req.originalUrl.substring(4, req.originalUrl.length));
-  var newUrl = apiServer + req.originalUrl.substring(4, req.originalUrl.length);
-  request({
-    "url" : newUrl,
-    "method" : req.method,
-    "json" : req.body,
-    "qs" : req.query,
-    "headers" : {
-      "Authorization" : req.headers.authorization ? req.headers.authorization : "" // if undefined, just place empty string
-    }
-  }).pipe(res);
+app.use('/api/*', function(req, res){
+  var path = url.parse(req.originalUrl).pathname;
+  var path = path.slice(4, path.length);
+  console.log(path);
+  var newUrl = req.originalUrl.slice(4, req.originalUrl.length);
+  proxy.web(req, res, { 'target' : apiServer + path});
 });
 
 app.listen(8000, function(){
-  console.log("Server running on port 8000...");
+  console.log('Server running on port 8000...');
 });
